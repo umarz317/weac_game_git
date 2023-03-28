2cd709e0a1197b8c45563ce3827289cfab82cab4
umarz317 <umar.317@gmail.com>
2023-03-29 02:40:14 +0500

    Correction of .js files

---
+++ web3/lib/web3modal.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ad19eab8465231703d1af715fb8fafa434515e733f3e7c945b2906c3c209b0fc
+size 428154
---
+++ web3/light.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dbea70db2ce1127aaae8604a7783086221af40194065b325670c7a3acaa5c038
+size 2910
