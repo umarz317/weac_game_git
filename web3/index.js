0718d25026896238d7bcfa6a473cc493b1f83040
umarz317 <61620968+umarz317@users.noreply.github.com>
2023-03-29 02:38:39 +0500

    Delete web3 directory

--- web3/index.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0e6f0274bab51e93d0cb1f7f5adef09e798c34a8ff09349c39fef179ba139713
-size 9419
--- web3/light.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dbea70db2ce1127aaae8604a7783086221af40194065b325670c7a3acaa5c038
-size 2910
